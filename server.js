553675a0983d6c563f8e9fd479a36c0c3f7bd907
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-04-14 14:29:03 -0500

    synciing with front end code

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -102,6 +104,22 @@ app.get('/api/animals/:id',(req, res) => {
   } else {
     res.send(404);
   } 
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
